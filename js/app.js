45831c250d6f9d5e948dc30a7b914932ac2791af
uniquejoel <joelsunday683@gmail.com>
2022-07-19 13:07:23 +0100

    pakiar upload

---
+++ js/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	$(window).on('scroll',function(){
+		var scroll = $(window).scrollTop();
+		if(scroll>=50){
+			$(".sticky").addClass("stickyadd");
+		}else{
+			$(".sticky").removeClass("stickyadd");
+		}
+	})
+
+
+
+});
